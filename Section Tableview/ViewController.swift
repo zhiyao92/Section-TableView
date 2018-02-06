b8c5a45c70889fd139424545fd5f10026a1f6683
Kelvin Tan <kelvintan@Kelvins-MacBook-Pro.local>
2018-02-06 09:52:09 +0800

    First Commit

--- Section Tableview/ViewController.swift
+++ Section Tableview/ViewController.swift
@@ -8,17 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var sectionHeader: Array<String> = ["Fruits","Vegetable","Snacks"]
+    var itemSection: Array<Array<String>> = [["Apple","Orange","Mango"], ["Cabbage","Tomatoes","Carrot","Cucumber","Spinach","Broccoli"] , ["Lays","Pringles","Pop Corn"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.sectionHeader.count
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.itemSection[section].count
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.sectionHeader[section]
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = self.itemSection[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = item
+        return cell
+    }
+    
 
 
 }
